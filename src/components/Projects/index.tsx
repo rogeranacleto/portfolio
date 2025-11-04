20befe27e464fc0cdb695501382a557e42b48c08
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-03 23:01:08 -0300

    style: add title section projects

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,11 @@
+export function Projects(){
+    return(
+        <div className="min-h-100 flex justify-center max-w-screen w-full">
+            <div className="flex flex-col items-center">
+                <h1 className="text-white text-5xl font-bold mb-3">Projetos</h1>
+                <div className="h-0.5 w-50 bg-linear-to-r from-transparent via-white/95 to-transparent mx-auto"></div>
+                <p className="text-[#c2bebee3] mt-4 text-lg">Alguns projetos que desenvolvi.</p>
+            </div>
+        </div>
+    )
+}
