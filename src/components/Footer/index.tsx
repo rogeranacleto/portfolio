d840a5e6d88afe54a894cbbf9956e96421cf801e
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-05 22:15:25 -0300

    style: change section footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,12 @@
 import { Links } from "../Links";
 export function Footer(){
     return(
-        <div className="min-h-90 bg-black border-t border-solid border-[#ffffff17] mt-60">
+        <div className="min-h-100 bg-black border-t border-solid border-[#ffffff17] mt-60">
             <div className="flex flex-col items-center justify-center mt-15">
-                <h2 className="text-[#813EEE] text-4xl font-bold">Vamos nos conectar?</h2>
+                <h2 className="text-[#3f00ac] text-4xl font-bold">Vamos nos conectar?</h2>
                 <p className="text-[#c2bebee3] text-lg mt-4 mb-5">Transformando ideias em realidade digital</p>
                 <Links/>
-                <div className="h-[0.5px] bg-[#c2bebe1f] w-[50%] mt-5 mb-5"></div>
+                <div className="h-[0.5px] bg-[#c2bebe1f] w-[50%] mt-10 mb-10"></div>
                 <p className="text-[#c2bebe7c]">© 2025 Roger Anacleto da Silva. Todos os direitos reservados.</p>
             </div>
         </div>
