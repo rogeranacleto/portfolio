846b15db53a12731e333a18e552bc92c261e8cc6
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-05 22:14:15 -0300

    style: change section about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,8 +8,8 @@ export function About(){
         <div className="min-h-100 flex justify-center max-w-screen w-full flex-col">
             <div className="flex flex-col">
                 <div className="flex flex-col items-center">
-                    <h1 className="text-white text-5xl font-bold mb-3">Sobre mim</h1>
-                    <div className="h-0.5 w-50 bg-linear-to-r from-transparent via-white/95 to-transparent mx-auto"></div>
+                    <h1 className="text-5xl font-bold mb-3 text-white">Sobre mim</h1>
+                    <div className="h-0.5 w-50 bg-linear-to-r from-transparent via-[#3f00ac] to-transparent mx-auto"></div>
                 </div>
                 <div className="flex items-center justify-center">
                     <p className="text-[#c2bebee3] mt-4 text-lg">Saiba mais sobre mim e minha carreira profissional.</p>
@@ -19,13 +19,13 @@ export function About(){
                 <div className="max-w-180 w-full min-h-150 backdrop-blur-xs border border-solid border-[#c2bebe15] rounded-lg p-10">
                     <h1 className="text-white font-bold text-3xl">Quem sou eu?</h1>
                     <p className="text-[#c2bebee3] mt-7 text-lg text-left">
-                    Sou Róger Anacleto da Silva, <span className="font-bold text-[#dad8d8]">Desenvolvedor Front-End</span> com formação em andamento em Análise e Desenvolvimento de Sistemas (ADS). 
+                    Sou Róger Anacleto da Silva, <span className="font-bold text-[#3f00ac]">Desenvolvedor Front-End</span> com formação em andamento em Análise e Desenvolvimento de Sistemas (ADS). 
                     Atuo no desenvolvimento de interfaces web que aliam boas práticas, acessibilidade e uma experiência de uso eficiente.
                     </p>
                     <p className="text-[#c2bebee3] mt-5 text-lg">Tenho como objetivo evoluir constantemente e contribuir para o desenvolvimento de soluções que resolvam problemas reais e gerem um impacto positivo.</p>
-                    <div className="h-0.5 w-100 bg-linear-to-r from-transparent via-white/95 to-transparent mx-auto mt-32"></div>
+                    <div className="h-0.5 w-100 bg-linear-to-r from-transparent via-[#3f00ac] to-transparent mx-auto mt-32"></div>
                     <div className="flex items-center justify-center mt-15 flex-col">
-                        <button className="bg-linear-to-r from-gray-600 to-white text-gray-900 font-semibold px-6 py-3 rounded-xl shadow-md hover:from-gray-700 hover:to-gray-200 transition-all hover:scale-110 duration-300 cursor-pointer">
+                        <button className="bg-linear-to-r from-[#3f00ac] via-[#3f00ac52] to-[#3f00ac1e] text-white font-semibold px-6 py-3 rounded-xl shadow-md hover:from-[#3f00ac] hover:to-[#3f00ac1e] transition-all hover:scale-110 duration-300 cursor-pointer">
                         Download Currículo
                         </button>
                         <p className="text-[#c2bebee3] text-sm mt-2">Baixe meu currículo em PDF.</p>
@@ -68,7 +68,7 @@ export function About(){
                             </div>
                         </div>
                     </div>
-                    <div className="h-0.5 w-100 bg-linear-to-r from-transparent via-white/95 to-transparent mx-auto mt-15"></div>
+                    <div className="h-0.5 w-100 bg-linear-to-r from-transparent via-[#3f00ac] to-transparent mx-auto mt-15"></div>
                     <div className="flex items-center justify-center mt-12 flex-col">
                         <Links/>
                         <p className="text-[#c2bebee3] text-sm mt-2">Vamos nos conectar?</p>
