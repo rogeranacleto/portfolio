64b78cc00d700393d3f287d4b1379ecc323679e8
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-05 22:40:53 -0300

    style: change section about

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -8,9 +8,39 @@ export function Hero(){
                 <div className="flex flex-col items-center justify-center">
                     <p className="text-white text-2xl mt-2">Desenvolvedor Front-End</p>
                     <Links/>
-                    <button className="mt-5 cursor-pointer">
-                        <span className="backdrop-blur-xs border border-solid border-[#c2bebe15] rounded-lg text-white pt-3 pb-3 pl-5 pr-5">Sobre mim</span>
-                    </button>
+                        <button
+                        className="
+                            relative mt-5 cursor-pointer
+                            overflow-hidden  /* <-- impede o vazamento do gradiente */
+                            rounded-lg
+                            group
+                        "
+                        >
+                        <span
+                            className="
+                            absolute inset-0
+                            bg-linear-to-r from-[#3f00ac] via-[#3700964d] to-[#3f00ac3b]
+                            -translate-x-full group-hover:translate-x-full
+                            transition-transform duration-700
+                            rounded-lg
+                            "
+                        ></span>
+                        <span
+                            className="
+                            relative z-10
+                            backdrop-blur-xs
+                            border border-solid border-[#c2bebe15]
+                            rounded-lg
+                            text-white
+                            pt-3 pb-3 px-5
+                            transition-all duration-500
+                            group-hover:border-[#3f00ac] group-hover:text-[#3f00ac] hover:font-bold
+                            block
+                            "
+                        >
+                            Sobre mim
+                        </span>
+                        </button>
                 </div>
             </div>
         </div>
