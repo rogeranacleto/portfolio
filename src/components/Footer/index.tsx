7708347450159d55b80217c20b4a40cbf5da35ba
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-05 03:39:18 -0300

    feat: change in footer section

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,13 @@
 import { Links } from "../Links";
+import { FiGithub } from "react-icons/fi";
+import { CiLinkedin } from "react-icons/ci";
+
 export function Footer(){
     return (
       <div className="min-h-140 bg-black  pt-20 pb-40 flex flex-col justify-center relative">
         <div className="absolute top-0 left-0 w-full flex justify-between items-start">
-          <div
-            className="h-px w-1/2 bg-linear-to-r from-white to-transparent rounded-full"
-          ></div>
-          <div
-            className="h-px w-1/2 bg-linear-to-l from-white to-transparent rounded-full"
-          ></div>
+          <div className="h-px w-1/2 bg-linear-to-r from-white to-transparent rounded-full"></div>
+          <div className="h-px w-1/2 bg-linear-to-l from-white to-transparent rounded-full"></div>
         </div>
         <div className="flex flex-col items-center justify-center mt-15 pl-10 pr-10">
           <h2 className="text-white text-5xl font-medium text-center mb-5">
@@ -19,10 +18,33 @@ export function Footer(){
             descobrir como podemos criar resultado juntos?
           </p>
           <Links />
-          <div className="h-[0.5px] bg-[#c2bebe1f] w-[50%] mt-10 mb-10"></div>
-          <p className="text-[#c2bebe7c] text-center">
-            © 2025 Roger Anacleto da Silva. Todos os direitos reservados.
-          </p>
+          <div className="h-px max-w-150 w-full bg-linear-to-r from-transparent via-white to-transparent mx-auto mt-10"></div>
+          <div
+            className="absolute bottom-2 flex items-center flex-col"
+            data-aos="fade-left"
+            data-aos-duration="2300"
+          >
+            <p className="text-white font-bold text-center">
+              Desenvolvido por Roger Anacleto • © 2025 Todos os direitos
+              reservados.
+            </p>
+            <div className="flex items-center gap-3 mt-2">
+              <a
+                href="https://github.com/rogeranacleto"
+                className="hover:scale-110 duration-300 ease-in-out"
+                target="_blank"
+              >
+                <FiGithub className="text-white text-lg" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/rogeranacleto/"
+                className="hover:scale-110 duration-300 ease-in-out"
+                target="_blank"
+              >
+                <CiLinkedin className="text-white text-2xl" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
