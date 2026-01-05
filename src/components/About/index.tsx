9cd12c7bb9db319e5cff932fa3e7e5ce5a95b445
Roger Anacleto <rogerdasilvacode7@gmail.com>
2026-01-05 05:09:09 -0300

    feat: correcting the resume link

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,7 +4,10 @@ import { FaCode } from "react-icons/fa6";
 import { RiLightbulbFlashLine } from "react-icons/ri";
 export function About(){
     return (
-      <div className="min-h-screen flex justify-center max-w-screen w-full flex-col mb-75" id="about">
+      <div
+        className="min-h-screen flex justify-center max-w-screen w-full flex-col mb-75"
+        id="about"
+      >
         <div className="flex flex-col items-center">
           <div className="flex flex-col items-center border border-solid border-black rounded-3xl bg-[#c2bebe15] max-w-30 mb-5">
             <h1 className="text-sm font-medium text-white pl-4 pr-4 pt-2 pb-2">
@@ -27,8 +30,8 @@ export function About(){
                 Desenvolvedor Front End
               </span>{" "}
               com formação em andamento em Análise e Desenvolvimento de Sistemas
-              (ADS). Atuo no desenvolvimento de interfaces web e procuro aliar boas
-              práticas, acessibilidade e uma experiência de uso eficiente.
+              (ADS). Atuo no desenvolvimento de interfaces web e procuro aliar
+              boas práticas, acessibilidade e uma experiência de uso eficiente.
             </p>
             <p className="text-black mt-5 text-lg">
               Tenho como objetivo evoluir constantemente e contribuir para o
@@ -36,9 +39,13 @@ export function About(){
               um impacto positivo.
             </p>
             <div className="flex items-center justify-center flex-col mt-10">
-              <button className="bg-linear-to-r from-[#1d1c1c] via-[#4b4a4a6b] to-[#c2bebe13] text-white font-semibold px-6 py-3 rounded-xl shadow-md transition-all hover:scale-110 duration-300 cursor-pointer">
+              <a
+                href="/curriculo-rogeranacleto.pdf"
+                target="_blank"
+                className="bg-linear-to-r from-[#1d1c1c] via-[#4b4a4a6b] to-[#c2bebe13] text-white font-semibold px-6 py-3 rounded-xl shadow-md transition-all hover:scale-110 duration-300 cursor-pointer"
+              >
                 Download Currículo
-              </button>
+              </a>
               <p className="text-[#c2bebee3] text-sm mt-2">
                 Baixe meu currículo em PDF.
               </p>
@@ -59,12 +66,15 @@ export function About(){
                   <h1 className="text-2xl text-white">Criação de Interfaces</h1>
                   <div>
                     <p className="text-[#c2bebee3] text-lg">
-                      Desenvolvo interfaces web com {" "}
+                      Desenvolvo interfaces web com{" "}
                       <span className="text-white font-bold">ReactJS</span>,{" "}
                       <span className="text-white font-bold">JavaScript</span>,{" "}
                       <span className="text-white font-bold">TypeScript </span>e
-                      <span className="text-white font-bold"> Tailwind CSS </span>
-                       criando experiências visuais atraentes e funcionais para
+                      <span className="text-white font-bold">
+                        {" "}
+                        Tailwind CSS{" "}
+                      </span>
+                      criando experiências visuais atraentes e funcionais para
                       todos os dispositivos.
                     </p>
                   </div>
